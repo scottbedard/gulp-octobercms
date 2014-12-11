370d4ae043f592b09d1f040597d426288f4b9714
Scott Bedard <scottbedard90@gmail.com>
2014-12-11 15:53:13 -0500

    Convert indentation to spaces

--- gulpfile.js
+++ gulpfile.js
@@ -15,34 +15,34 @@ var groupmedia = require('gulp-group-css-media-queries');
  * SCSS / CSS processes
  */
 gulp.task('css', function() {
-	gulp.src('assets/scss/main.scss')
-		.pipe(plumber({								// Logs errors and makes a beep
-			errorHandler: function (err) {  
-				gutil.beep();
-				console.log(err);
-			}
-		}))
-		.pipe(sass())								// Compile our SCSS
-		.pipe(autoprefixer('last 20 versions'))		// Add browser prefixes
-		.pipe(groupmedia())							// Group media queries
-		.pipe(uncss({
-			html: glob.sync('**/*.htm')				// Scan the theme and remove unnecessary selectors
-		}))
-		.pipe(minifycss())							// Minify
-		.pipe(gulp.dest('assets/css'));				// Pipe the output to our css directory
+    gulp.src('assets/scss/main.scss')
+        .pipe(plumber({                             // Logs errors and makes a beep
+            errorHandler: function (err) {  
+                gutil.beep();
+                console.log(err);
+            }
+        }))
+        .pipe(sass())                               // Compile our SCSS
+        .pipe(autoprefixer('last 20 versions'))     // Add browser prefixes
+        .pipe(groupmedia())                         // Group media queries
+        .pipe(uncss({
+            html: glob.sync('**/*.htm')             // Scan the theme and remove unnecessary selectors
+        }))
+        .pipe(minifycss())                          // Minify
+        .pipe(gulp.dest('assets/css'));             // Pipe the output to our css directory
 });
 
 /**
  * Watches the file system for changes
  */
 gulp.task('watch', function() {
-	gulp.watch([
-		'content/*.htm',							// Watch OctoberCMS .htm files
-		'layouts/*.htm',
-		'pages/*.htm',
-		'partials/*.htm',
-		'assets/scss/**/*.scss'						// Watch for .scss files
-	], ['css']);
+    gulp.watch([
+        'content/*.htm',                            // Watch OctoberCMS .htm files
+        'layouts/*.htm',
+        'pages/*.htm',
+        'partials/*.htm',
+        'assets/scss/**/*.scss'                     // Watch for .scss files
+    ], ['css']);
 })
 
 gulp.task('default', ['watch']);
